7258b5f8a0dae3edadd6841e450f7c14f2d176f7
jaemsz <68905231+jaemsz@users.noreply.github.com>
2021-03-27 16:42:32 -0700

    Update COutput.h

--- interview2/interview2/COutput.h
+++ interview2/interview2/COutput.h
@@ -3,26 +3,26 @@
 #include <windows.h>
 
 /*
-*	Definition of COutput
+*   Definition of COutput
 */
 class COutput
 {
 public:
-	COutput();
-	~COutput();
+    COutput();
+    ~COutput();
 
-	bool update(int nIndex);
-	void display(void);
+    bool update(int nIndex);
+    void display(void);
 
-	// Used for unit testing
-	int sumFreq(void);
+    // Used for unit testing
+    int sumFreq(void);
 
 private:
-	std::vector<int>	m_vFrequency;
+    std::vector<int>    m_vFrequency;
 
-	// Decided to use a critical section since
-	// we just need to synchronize access to shared
-	// data between threads in a single process
-	CRITICAL_SECTION	m_cs;
+    // Decided to use a critical section since
+    // we just need to synchronize access to shared
+    // data between threads in a single process
+    CRITICAL_SECTION    m_cs;
 };
 
