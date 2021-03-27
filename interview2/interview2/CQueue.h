cef2da581f5e076585cc8dea71e48bbafbc5eeb2
jaemsz <68905231+jaemsz@users.noreply.github.com>
2021-03-27 16:43:03 -0700

    Update CQueue.h

--- interview2/interview2/CQueue.h
+++ interview2/interview2/CQueue.h
@@ -6,37 +6,37 @@
 // and pop methods
 enum class QueueRet
 {
-	ok,		// Successfully pushed or popped an item
-	empty,	// Queue is empty
-	full,	// Queue is full
-	term,	// Queue has seen N items
+    ok,     // Successfully pushed or popped an item
+    empty,  // Queue is empty
+    full,   // Queue is full
+    term,   // Queue has seen N items
 };
 
 /*
-*	Definition of CQueue
+*   Definition of CQueue
 */
 class CQueue
 {
 public:
-	// The queues max size.  It should be 10.
-	static const int	m_nQueueMaxSize;
+    // The queues max size.  It should be 10.
+    static const int m_nQueueMaxSize;
 
-	CQueue();
-	CQueue(int nMaxValues);
-	~CQueue();
+    CQueue();
+    CQueue(int nMaxValues);
+    ~CQueue();
 
-	QueueRet push(int val);
-	QueueRet pop(int& val);
+    QueueRet push(int val);
+    QueueRet pop(int& val);
 
 private:
-	bool				m_bTerm;
-	int					m_nMaxValues;	// This is N
-	int					m_nCount;
-	std::queue<int>		m_queue;
+    bool                m_bTerm;
+    int                 m_nMaxValues;   // This is N
+    int                 m_nCount;
+    std::queue<int>     m_queue;
 
-	// Decided to use a critical section since
-	// we just need to synchronize access to shared
-	// data between threads in a single process
-	CRITICAL_SECTION	m_cs;
+    // Decided to use a critical section since
+    // we just need to synchronize access to shared
+    // data between threads in a single process
+    CRITICAL_SECTION    m_cs;
 };
 
