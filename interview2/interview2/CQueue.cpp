6d8c9949a2dee1220f9336ea33b57b27dd5974e8
jaemsz <68905231+jaemsz@users.noreply.github.com>
2021-03-27 16:41:57 -0700

    Update CQueue.cpp

--- interview2/interview2/CQueue.cpp
+++ interview2/interview2/CQueue.cpp
@@ -4,118 +4,120 @@
 const int CQueue::m_nQueueMaxSize = 10;
 
 /*
-*	CQueue default constructor initializes
-*	member variables
+*   CQueue default constructor initializes
+*   member variables
 */
 CQueue::CQueue() :
-	m_bTerm(false),
-	m_nCount(0),
-	m_nMaxValues(10)
+    m_bTerm(false),
+    m_nCount(0),
+    m_nMaxValues(10)
 {
-	InitializeCriticalSection(&m_cs);
+    InitializeCriticalSection(&m_cs);
 }
 
 /*
-*	CQueue constructor initializes
-*	member variables
+*   CQueue constructor initializes
+*   member variables
 * 
-*	@param nMaxValues : This value represents N
+*   @param nMaxValues : This value represents N
 */
 CQueue::CQueue(int nMaxValues) : 
-	m_bTerm(false),
-	m_nCount(0), 
-	m_nMaxValues(nMaxValues)
+    m_bTerm(false),
+    m_nCount(0), 
+    m_nMaxValues(nMaxValues)
 {
-	InitializeCriticalSection(&m_cs);
+    InitializeCriticalSection(&m_cs);
 }
 
 /*
-*	CQueue destructor performs clean up
+*   CQueue destructor performs clean up
 */
 CQueue::~CQueue()
 {
-	// Delete the critical section object
-	DeleteCriticalSection(&m_cs);
+    // Delete the critical section object
+    DeleteCriticalSection(&m_cs);
 }
 
 /*
-*	The push function adds a value to the queue
+*   The push function adds a value to the queue
 * 
-*	@param val : A value to add to the queue
-*	@return : 
-*		QueueRet::ok   : val was added successuflly
-*		QueueRet::full : queue is full
-*		QueueRet:term  : queue has seen N items
+*   @param val : A value to add to the queue
+*   @return : 
+*       QueueRet::ok    : val was added successuflly
+*       QueueRet::full  : queue is full
+*       QueueRet:term   : queue has seen N items
 */
 QueueRet CQueue::push(int val)
 {
-	// This object can be called from multiple threads
-	// so we protect shared data with a critical section
-	EnterCriticalSection(&m_cs);
+    // This object can be called from multiple threads
+    // so we sychronize access to shared data with a 
+    // critical section
+    EnterCriticalSection(&m_cs);
 
-	if (m_nCount == m_nMaxValues)
-	{
-		// N values have been seen.
-		// Save this state to m_bTerm
-		m_bTerm = true;
-		LeaveCriticalSection(&m_cs);
-		// Let the caller terminate
-		return QueueRet::term;
-	}
+    if (m_nCount == m_nMaxValues)
+    {
+        // N values have been seen.
+        // Save this state to m_bTerm
+        m_bTerm = true;
+        LeaveCriticalSection(&m_cs);
+        // Let the caller terminate
+        return QueueRet::term;
+    }
 
-	if (m_queue.size() < m_nQueueMaxSize)
-	{
-		// Add val to the queue
-		m_queue.push(val);
-		// Increment our counter
-		m_nCount++;
+    if (m_queue.size() < m_nQueueMaxSize)
+    {
+        // Add val to the queue
+        m_queue.push(val);
+        // Increment our counter
+        m_nCount++;
 
-		LeaveCriticalSection(&m_cs);
-		// Let the caller know that val was added
-		return QueueRet::ok;
-	}
+        LeaveCriticalSection(&m_cs);
+        // Let the caller know that val was added
+        return QueueRet::ok;
+    }
 
-	LeaveCriticalSection(&m_cs);
-	return QueueRet::full;
+    LeaveCriticalSection(&m_cs);
+    return QueueRet::full;
 }
 
 /*
-*	The pop function returns a value from the queue.
+*   The pop function returns a value from the queue.
 * 
-*	@param val : A value from the queue
-*	@return :
-*		QueueRet::ok    : val was popped successuflly
-*		QueueRet::empty : queue is empty
-*		QueueRet:term   : queue has seen N items
+*   @param val : A value from the queue
+*   @return :
+*       QueueRet::ok    : val was popped successuflly
+*       QueueRet::empty : queue is empty
+*       QueueRet:term   : queue has seen N items
 */
 QueueRet CQueue::pop(int& val)
 {
-	// This object can be called from multiple threads
-	// so we protect shared data with a critical section
-	EnterCriticalSection(&m_cs);
+    // This object can be called from multiple threads
+    // so we sychronize access to shared data with a 
+    // critical section
+    EnterCriticalSection(&m_cs);
 
-	if (m_queue.empty())
-	{
-		if (m_bTerm)
-		{
-			// The queue has seen N values and there
-			// are no more items to process on the queue
-			LeaveCriticalSection(&m_cs);
-			// Let the caller terminate
-			return QueueRet::term;
-		}
+    if (m_queue.empty())
+    {
+        if (m_bTerm)
+        {
+            // The queue has seen N values and there
+            // are no more items to process on the queue
+            LeaveCriticalSection(&m_cs);
+            // Let the caller terminate
+            return QueueRet::term;
+        }
 
-		LeaveCriticalSection(&m_cs);
-		// Tell the caller that the queue is empty
-		return QueueRet::empty;
-	}
+        LeaveCriticalSection(&m_cs);
+        // Tell the caller that the queue is empty
+        return QueueRet::empty;
+    }
 
-	// Take a value from the queue
-	// and save it to val
-	val = m_queue.front();
-	m_queue.pop();
+    // Take a value from the queue
+    // and save it to val
+    val = m_queue.front();
+    m_queue.pop();
 
-	LeaveCriticalSection(&m_cs);
-	// Let the caller know that pop succeeded
-	return QueueRet::ok;
+    LeaveCriticalSection(&m_cs);
+    // Let the caller know that pop succeeded
+    return QueueRet::ok;
 }
