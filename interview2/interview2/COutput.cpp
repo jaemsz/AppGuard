135e50bc000ebdfa611d14324d18094c613cff2a
jaemsz <68905231+jaemsz@users.noreply.github.com>
2021-03-27 16:41:37 -0700

    Update COutput.cpp

--- interview2/interview2/COutput.cpp
+++ interview2/interview2/COutput.cpp
@@ -2,83 +2,90 @@
 #include <iostream>
 
 /*
-*	COutput default constructor initializes
-*	member variables
+*   COutput default constructor initializes
+*   member variables
 */
 COutput::COutput()
 {
-	InitializeCriticalSection(&m_cs);
+    InitializeCriticalSection(&m_cs);
 
-	// Frequency vector should hold up to 10 
-	// values:  0 to 9
-	m_vFrequency = { 0,0,0,0,0,0,0,0,0,0 };
+    // Frequency vector should hold up to 10 
+    // values:  0 to 9
+    m_vFrequency = { 0,0,0,0,0,0,0,0,0,0 };
 }
 
 /*
-*	COutput destructor performs clean up
+*   COutput destructor performs clean up
 */
 COutput::~COutput()
 {
-	DeleteCriticalSection(&m_cs);
+    DeleteCriticalSection(&m_cs);
 }
 
 /*
-*	The update method updates the frequency vector
+*   The update method updates the frequency vector
 * 
-*	@param nIndex : an index
+*   @param nIndex : an index
 */
 bool COutput::update(int nIndex)
 {
-	if (nIndex < 0 || nIndex >= 10)
-	{
-		// If input value is not valid,
-		// return false
-		return false;
-	}
+    if (nIndex < 0 || nIndex >= 10)
+    {
+        // If input value is not valid,
+        // return false
+        return false;
+    }
 
-	// Protect access to shared data because
-	// multiple threads will call this method
-	EnterCriticalSection(&m_cs);
-	// incrememt count at the index
-	m_vFrequency[nIndex] += 1;
-	LeaveCriticalSection(&m_cs);
+    // This object can be called from multiple threads
+    // so we sychronize access to shared data with a 
+    // critical section
+    EnterCriticalSection(&m_cs);
+    // incrememt count at the index
+    m_vFrequency[nIndex] += 1;
+    LeaveCriticalSection(&m_cs);
 
-	return true;
+    return true;
 }
 
 /*
-*	The display method outputs frequency vector
-*	to stdout
+*   The display method outputs frequency vector
+*   to stdout
 */
 void COutput::display(void)
 {
-	EnterCriticalSection(&m_cs);
+    // This object can be called from multiple threads
+    // so we sychronize access to shared data with a 
+    // critical section
+    EnterCriticalSection(&m_cs);
 
-	int i = 0;
-	for (int nVal : m_vFrequency)
-	{
-		std::cout << "index = " << i << " : " << nVal << std::endl;
-		i++;
-	}
+    int i = 0;
+    for (int nVal : m_vFrequency)
+    {
+        std::cout << "index = " << i << " : " << nVal << std::endl;
+        i++;
+    }
 
-	LeaveCriticalSection(&m_cs);
+    LeaveCriticalSection(&m_cs);
 }
 
 /*
-*	The sumFreq method is used by unit tests
-*	to verify that the frequency counts add
-*	up to N.
+*   The sumFreq method is used by unit tests
+*   to verify that the frequency counts add
+*   up to N.
 */
 int COutput::sumFreq(void)
 {
-	EnterCriticalSection(&m_cs);
+    // This object can be called from multiple threads
+    // so we sychronize access to shared data with a 
+    // critical section
+    EnterCriticalSection(&m_cs);
 
-	int nAccum = 0;
-	for (int nVal : m_vFrequency)
-	{
-		nAccum += nVal;
-	}
+    int nAccum = 0;
+    for (int nVal : m_vFrequency)
+    {
+        nAccum += nVal;
+    }
 
-	LeaveCriticalSection(&m_cs);
-	return nAccum;
+    LeaveCriticalSection(&m_cs);
+    return nAccum;
 }
